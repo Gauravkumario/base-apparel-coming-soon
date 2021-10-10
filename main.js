34b784b964eadbeb6ff3966297c82a3c300ae354
Gaurav Kumar <50291149+Gauravkumario@users.noreply.github.com>
2021-10-10 19:40:52 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
-console.log("hello");
+const btn = document.querySelector('#arrow-logo');
+btn.addEventListener('click', () => {
+    validation();
+})
+
+function validation() {
+    var form = document.getElementById('form');
+    var email = document.getElementById('email').value;
+    var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+    if (email.match(pattern))
+    {
+        document.getElementById('submit-txt').style.display = "block";
+        document.getElementById('error-txt').style.display = "";
+    }
+
+    else if(email.length == 0)
+    {
+        document.getElementById('user-mail').style.border = ""
+        document.getElementById('error-txt').style.display = "";
+        document.getElementById('submit-txt').style.display = "";
+        document.getElementById('error-logo').style.display = ""
+    }
+
+    else
+    {
+        document.getElementById('user-mail').style.border = "2px solid hsl(0, 93%, 68%)"
+        document.getElementById('error-txt').style.display = "block";
+        document.getElementById('error-logo').style.display = "flex"
+        document.getElementById('submit-txt').style.display = "";
+    }
+}
